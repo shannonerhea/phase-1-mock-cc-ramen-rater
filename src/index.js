579b66c4d9976dd5b9715c72ba0e9ba706a4c20d
shannone <shannone426@gmail.com>
2022-08-17 10:57:04 -0400

    saving

--- src/index.js
+++ src/index.js
@@ -9,10 +9,7 @@ As a user, I can:
 - Click on an image from the `#ramen-menu` div and see all the info about that
   ramen displayed inside the `#ramen-detail` div and where it says
   `insert comment here` and `insert rating here`.
-- Create a new ramen after submitting the `new-ramen` form. The new ramen should
-  be added to the`#ramen-menu` div. The new ramen does not need to persist; in
-  other words, if you refresh the page, it's okay that the new ramen is no
-  longer on the page. */
+ */
 let currentItem;
 fetch('http://localhost:3000/ramens')
 .then(response => response.json())
@@ -24,7 +21,7 @@ fetch('http://localhost:3000/ramens')
 })
 
 function ramenMenu(data) {
-    const menu = document.querySelector(`ramen-menu`);
+    const menu = document.querySelector(`#ramen-menu`);
 
     data.forEach(item => {
         const menuItem = document.createElement('span')
@@ -37,4 +34,16 @@ function ramenMenu(data) {
 
 function selectItem(item) {
     currentItem = item
+    
 };
+
+/**Create a new ramen after submitting the `new-ramen` form. The new ramen should
+be added to the`#ramen-menu` div. The new ramen does not need to persist; in
+other words, if you refresh the page, it's okay that the new ramen is no
+longer on the page.  */
+const ramenForm = document.querySelector('#new-ramen')
+ramenForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+
+})
